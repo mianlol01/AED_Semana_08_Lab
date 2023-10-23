ad62e797a5ed1328a21e50138c2a750e74962038
mianlol01 <snake.1503@outlook.com>
2023-10-23 15:09:49 -0500

    segundo problema resuelto :o

--- Problema_8_2/src/semana_08/Arreglo.java
+++ Problema_8_2/src/semana_08/Arreglo.java
@@ -1,37 +1,61 @@
 package semana_08;
 
+import java.util.Arrays;
+
 public class Arreglo {
-	
-	//  Atributos privados
+
+	// Atributos privados
 	private int[] n;
 	private int indice;
-	//  Constructor
+
+	// Constructor
 	public Arreglo() {
 		n = new int[10];
 		indice = 0;
 	}
-	//  Operaciones públicas básicas
+
+	// Operaciones pï¿½blicas bï¿½sicas
 	public int tamanio() {
 		return indice;
 	}
+
 	public int obtener(int i) {
 		return n[i];
 	}
-	public void adicionarDistintoYordenar(int numero) {
 
+	public void adicionarDistintoYordenar(int numero) {
+		if (indice == n.length) {
+			ampliarArreglo();
+		}
+		if (!existeNumero(numero)) {
+			n[indice] = numero;
+			indice++;
+			Arrays.sort(n, 0, indice);
+		}
 	}
-	//  Operaciones privadas
+
+	// Operaciones privadas
 	private void ampliarArreglo() {
 		int[] aux = n;
 		n = new int[indice + 10];
-		for (int i=0; i<indice; i++)
+		for (int i = 0; i < indice; i++)
 			n[i] = aux[i];
 	}
+
 	public void adicionar(int numero) {
 		if (indice == n.length)
 			ampliarArreglo();
 		n[indice] = numero;
-		indice ++;
+		indice++;
 	}
-	
+
+	private boolean existeNumero(int numero) {
+		for (int i = 0; i < indice; i++) {
+			if (n[i] == numero) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
