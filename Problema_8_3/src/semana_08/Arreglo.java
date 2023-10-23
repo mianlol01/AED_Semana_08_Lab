7b93f02d8ca66d708df3babaf03ee52ed1a3b6ff
mianlol01 <snake.1503@outlook.com>
2023-10-23 15:21:04 -0500

    tercer problema resuelto dasdq

--- Problema_8_3/src/semana_08/Arreglo.java
+++ Problema_8_3/src/semana_08/Arreglo.java
@@ -1,37 +1,53 @@
 package semana_08;
 
 public class Arreglo {
-	
-	//  Atributos privados
+
+	// Atributos privados
 	private int[] n;
 	private int indice;
-	//  Constructor
+
+	// Constructor
 	public Arreglo() {
 		n = new int[10];
 		indice = 0;
 	}
-	//  Operaciones públicas básicas
+
+	// Operaciones pï¿½blicas bï¿½sicas
 	public int tamanio() {
 		return indice;
 	}
+
 	public int obtener(int i) {
 		return n[i];
 	}
-	public void adicionarYreubicar(int numero) {
 
+	public void adicionarYreubicar(int numero) {
+		if (indice == n.length)
+			ampliarArreglo();
+		if (numero % 2 == 0) {
+			for (int i = indice; i > 0; i--) {
+				n[i] = n[i - 1];
+			}
+			n[0] = numero;
+		} else {
+			n[indice] = numero;
+		}
+		indice++;
 	}
-	//  Operaciones privadas
+
+	// Operaciones privadas
 	private void ampliarArreglo() {
 		int[] aux = n;
 		n = new int[indice + 10];
-		for (int i=0; i<indice; i++)
+		for (int i = 0; i < indice; i++)
 			n[i] = aux[i];
 	}
+
 	public void adicionar(int numero) {
 		if (indice == n.length)
 			ampliarArreglo();
 		n[indice] = numero;
-		indice ++;
+		indice++;
 	}
-	
+
 }
